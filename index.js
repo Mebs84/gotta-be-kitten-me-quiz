5da24284844dc6e112b127f32bffbe1ae7c27be1
Meghan Vining <vine.meg84@gmail.com>
2019-07-10 05:55:59 -0600

    first js commit

---
+++ index.js
@@ -0,0 +1,159 @@
+let QUIZ = [
+    {
+        question_text:"Kittens are unable to retract their claws until they ____ weeks of age.",
+        answers: [
+            "6 weeks",
+            "1 week",
+            "4 weeks",
+            "8 weeks"
+        ],
+        correctAnswer:"4 weeks",
+         image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+          alt:"kitten mewing"
+    },
+    {
+        quesion_text: "All kittens are born with ___ color eyes.",
+        answers: [
+            "Blue",
+            "Green",
+            "Brown",
+            "Purple"
+        ],
+        correctAnswer: "Blue",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+         alt: "kitten mewing"
+    },
+    {
+        question_text: "Cats sense of ___ is one of the best in the world.",
+        answers: [
+            "Smell",
+            "Hearing",
+            "Sight",
+            "Taste"
+        ],
+        correctAnswer: "Hearing",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        question_text: "Kittens are fully weaned at ___ weeks old.",
+        answers: [
+            "12",
+            "2",
+            "8",
+            "4"
+        ],
+        correctAnswer: "8",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    { 
+        question_text: "Male cats are called ___.",
+        answers: [
+            "Billies",
+            "Freds",
+            "Jakes",
+            "Toms"
+        ],
+        correctAnswer: "Toms",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        question_text: "Female cats are called ___.",
+        answers: [
+            "Queens",
+            "Shebas",
+            "Maids",
+            "Cleos"
+        ],
+        correctAnswer: "Queens",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        questions_text: "A group of cats is called a ___.",
+        answers: [
+            "Murder",
+            "Clowder",
+            "Passle",
+            "Flock"
+        ],
+        correctAnswer: "Clowder",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        question_text: "There are approximately ___ different breeds of cat in the world.",
+        answers: [
+            "120",
+            "15",
+            "70",
+            "40"
+        ],
+        correctAnswer: "70",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        question_text: "On average a cats sleep about ___ hours per day.",
+        answers: [
+            "5",
+            "20",
+            "16",
+            "10",
+        ],
+        correctAnswer: "16",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+    {
+        question_text: "A haw is the third ___ of a cat.",
+        answers: [
+            "Eyelid",
+            "Front toe",
+            "Lip",
+            "Ear",
+        ],
+        correctAnswer: "Eyelid",
+        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjj8_LSsZ7jAhVECc0KHZKiBN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.thehappycatsite.com%2Fwhen-do-cats-stop-growing%2F&psig=AOvVaw3JMXMjc4OC3fc3YA0b6Hmx&ust=1562437027702191",
+        alt: "kitten mewing"
+    },
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+
+function startQuizButton() {
+    //when the start quiz button is clicked
+    //will begin questions
+    $(".buttonStart").on("click", "button.startButton", function() {
+        console.log('begin');
+    });
+}
+
+startQuizButton();
+
+function renderCurrentQuestionToHTML() {
+    // look at the data for the current question
+    // and render it to HTML
+    let q = QUIZ[currentQuestion];
+}
+
+function goForwardOnePage() {
+    // increment forward one page
+    currentQuestion++;
+}
+
+function checkUserAnswer() {
+    // check if the user's answers
+    // matches the correct answer
+
+}
+
+function updateScore() {
+    // increment the score based on
+    // correct or incorrect answers
+
+}
